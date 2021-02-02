13d524153247cb857be36881fb68dbdb8cda1d9c
soichiro0915 <soichiro.ex.eru0915@gmail.com>
2021-02-02 13:24:47 +0900

    2/1

--- AtCoder/ABC/25/A.cpp
+++ AtCoder/ABC/25/A.cpp
@@ -1,4 +1,20 @@
-#include <bits/stdc++.h>
+#include <iostream> // cout, endl, cin
+#include <string> // string, to_string, stoi
+#include <vector> // vector
+#include <algorithm> // min, max, swap, sort, reverse, lower_bound, upper_bound
+#include <utility> // pair, make_pair
+#include <tuple> // tuple, make_tuple
+#include <cstdint> // int64_t, int*_t
+#include <cstdio> // printf
+#include <map> // map
+#include <queue> // queue, priority_queue
+#include <set> // set
+#include <stack> // stack
+#include <deque> // deque
+#include <unordered_map> // unordered_map
+#include <unordered_set> // unordered_set
+#include <bitset> // bitset
+#include <cctype> // isupper, islower, isdigit, toupper, tolower
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define _GLIBCXX_DEBUG
