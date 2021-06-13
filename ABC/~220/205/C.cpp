c5bf4d6fd28304608d5bebfb1ec05a76aff8fbfd
soichiro0915 <soichiro.ex.eru0915@gmail.com>
2021-06-13 22:38:36 +0900

    ABC205

--- ABC/~220/205/C.cpp
+++ ABC/~220/205/C.cpp
@@ -7,12 +7,60 @@ using P = pair<int, int>;
 
 int main() {
   int a, b, c;
-  ll ac, bc;
   cin >> a >> b >> c;
 
-
-
-  if(log(a) < log(b)) cout << "<" << endl;
+  if(a == 0 && b != 0){
+    if(b > 0){
+      cout << "<" << endl;
+    }else {
+      if(c % 2 == 0){
+        cout << "<" << endl;
+      }else cout << ">" << endl;
+    }
+  }
+  else if(b == 0 && a != 0){
+    if(a > 0){
+      cout << ">" << endl;
+    }else {
+      if(c % 2 == 0){
+        cout << ">" << endl;
+      }else cout << "<" << endl;
+    }
+  }
+  else if(a == 0 && b == 0){
+    cout << "=" << endl;
+  }
+  else if(a > 0 && b > 0){
+    if(log(a) < log(b)) cout << "<" << endl;
   else if(log(a) > log(b)) cout << ">" << endl;
   else cout << "=" << endl;
+  } else if(a < 0 && b > 0) {
+    if(c % 2 == 0){
+      if(log(-a) < log(b)) cout << "<" << endl;
+      else if(log(-a) > log(b)) cout << ">" << endl;
+      else cout << "=" << endl;
+    } else {
+      cout << "<" << endl;
+    }
+  } else if(a > 0 && b < 0) {
+    if(c % 2 == 0){
+      if(log(a) < log(-b)) cout << "<" << endl;
+      else if(log(a) > log(-b)) cout << ">" << endl;
+      else cout << "=" << endl;
+    } else {
+      cout << ">" << endl;
+      }
+  } else if(a < 0 && b < 0){
+    if(c % 2 == 0){
+      if(-a < -b) cout << "<" << endl;
+      else if(-a > -b) cout << ">" << endl;
+      else cout << "=" << endl;
+    }
+    else{
+      if(-a > -b) cout << "<" << endl;
+      else if(-a < -b) cout << ">" << endl;
+      else cout << "=" << endl;
+    }
+  }
 }
+
